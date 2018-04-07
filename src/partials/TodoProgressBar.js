f1948de51b9c2c218ea68c275d5be228fa568002
Jonathan Mak <jonathan.mak@liferay.com>
2018-04-06 17:03:49 -0700

    Update Todo App blog

--- src/partials/TodoProgressBar.js
+++ src/partials/TodoProgressBar.js
@@ -6,11 +6,9 @@ import Soy from 'metal-soy';
 import templates from './TodoProgressBar.soy.js';
 
 class TodoProgressBar extends Component {
-
 }
 
 TodoProgressBar.STATE = {
-
 };
 
 Soy.register(TodoProgressBar, templates);
